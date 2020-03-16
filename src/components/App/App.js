cd6217628a7caefb8277e85601f61be43b97cf91
Edward Stoner <edwardlstoner@gmail.com>
2020-03-16 17:26:18 -0600

    Pass in data as props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import local from '../../data/local';
-import './App.css';
+import React, { Component } from "react";
+import local from "../../data/local";
+import "./App.css";
+import NewsContainer from "../NewsContainer/NewsContainer";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       local
-    }
+    };
   }
 
-  render () {
+  render() {
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+        <NewsContainer articles={local} />
       </div>
     );
   }
