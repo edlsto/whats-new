1bff70ccb199f16ba7568d56db7c66f389b2b845
Edward Stoner <edwardlstoner@gmail.com>
2020-03-16 17:26:57 -0600

    Create NewsArticle component

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,16 @@
-import React from 'react';
-import './NewsContainer.css'
+import React from "react";
+import "./NewsContainer.css";
+import NewsArticle from "../NewsArticle/NewsArticle";
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = props => {
+  return props.articles.map(article => (
+    <NewsArticle
+      headline={article.headline}
+      description={article.description}
+      url={article.url}
+      img={article.img}
+    />
+  ));
+};
 
 export default NewsContainer;
