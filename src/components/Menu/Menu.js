4ea2079a015262ce9870a20454cf355ba18f5d72
Edward Stoner <edwardlstoner@gmail.com>
2020-03-17 15:58:31 -0600

    Add Menu component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,36 @@
-import React from 'react';
-import './Menu.css'
+import React, { Component } from "react";
+import "./Menu.css";
 
-// MENU COMPONENT CODE GOES HERE
+class Menu extends Component {
+  constructor(props) {
+    super();
+  }
+
+  handleClick = e => {
+    this.props.selectTopic(e.target.getAttribute("section"));
+  };
+
+  render() {
+    return (
+      <aside>
+        <div onClick={e => this.handleClick(e)} section="local">
+          Local news
+        </div>
+        <div onClick={e => this.handleClick(e)} section="technology">
+          Technology
+        </div>
+        <div onClick={e => this.handleClick(e)} section="entertainment">
+          Entertainment
+        </div>
+        <div onClick={e => this.handleClick(e)} section="science">
+          Science
+        </div>
+        <div onClick={e => this.handleClick(e)} section="health">
+          Health
+        </div>
+      </aside>
+    );
+  }
+}
 
 export default Menu;
