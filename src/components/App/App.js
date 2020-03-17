ace55f778f54df7ccdb66828c9a181c1b2192631
Edward Stoner <edwardlstoner@gmail.com>
2020-03-16 21:45:41 -0600

    Add sidebar and additional data sources

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import local from "../../data/local";
+import entertainment from "../../data/entertainment";
+import health from "../../data/health";
+import technology from "../../data/technology";
+import science from "../../data/science";
 import "./App.css";
 import NewsContainer from "../NewsContainer/NewsContainer";
 
@@ -7,14 +11,30 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local: local,
+      entertainment: entertainment,
+      health: health,
+      technology: technology,
+      science: science
     };
   }
 
   render() {
     return (
-      <div className="app">
-        <NewsContainer articles={local} />
+      <div>
+        <header>What's New</header>
+        <main>
+          <aside>
+            <div>Local News</div>
+            <div>Technology</div>
+            <div>Entertainment</div>
+            <div>Science</div>
+            <div>Health</div>
+          </aside>
+          <div className="app">
+            <NewsContainer articles={this.state.local} />
+          </div>
+        </main>
       </div>
     );
   }
