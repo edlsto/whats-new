29fa4a66f51a4417d9a6698d0e12baebf18b8e06
Edward Stoner <edwardlstoner@gmail.com>
2020-03-16 21:46:16 -0600

    Fix elements for NewsArticle component

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./NewsArticle.css";
 
 const NewsArticle = props => (
-  <div>
-    <div>{props.headline}</div>
-    <div>{props.description}</div>
-    <div>{props.url}</div>
-    <div>
-      <img src={props.img} alt="" width="100" height="100" />
-    </div>
+  <div class="news-article">
+    <img src={props.img} alt="" />
+    <h2>{props.headline}</h2>
+    <p>{props.description}</p>
+    <button>
+      <a href={props.url}>Read more</a>
+    </button>
   </div>
 );
 
