3bbb5a55509a9a8ca0fc167c7cbe10ad959184a9
Edward Stoner <edwardlstoner@gmail.com>
2020-03-17 20:57:38 -0600

    Create Search Form component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,43 @@
-import React from 'react';
-import './SearchForm.css';
+import React, { Component } from "react";
+import "./SearchForm.css";
 
-// SEARCHFORM COMPONENT CODE GOES HERE
+class SearchForm extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      searchTerm: ""
+    };
+  }
+
+  updateState = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  handleKeyUp = event => {
+    if (event.which === 13) {
+      this.props.searchStories(this.state.searchTerm);
+    }
+  };
+
+  submitSearchTerm = event => {
+    event.preventDefault();
+    this.props.searchStories(this.state.searchTerm);
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          placeholder="Search"
+          onChange={this.updateState}
+          onKeyUp={this.handleKeyUp}
+        ></input>
+        <button className="search-btn" onClick={this.submitSearchTerm}>
+          Search
+        </button>
+      </div>
+    );
+  }
+}
 
 export default SearchForm;
