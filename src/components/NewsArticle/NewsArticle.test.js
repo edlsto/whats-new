c9ac42fa3b13c0bf044e4e155370d9424ea19643
Edward Stoner <edwardlstoner@gmail.com>
2020-03-19 15:36:41 -0600

    Add more testing for NewsArticle

--- src/components/NewsArticle/NewsArticle.test.js
+++ src/components/NewsArticle/NewsArticle.test.js
@@ -4,15 +4,23 @@ import NewsArticle from "./NewsArticle.js";
 import "@testing-library/jest-dom";
 
 describe("NewsArticle", () => {
-  it("renders text that we expect", () => {
-    const { getByText } = render(
+  it("text of headline and description are rendered", () => {
+    const { getByText, debug } = render(
       <NewsArticle
         headline={"Here is a headline"}
         description={"My description goes here"}
         url={"http://www.denverpost.com"}
+        img={
+          "https://static01.nyt.com/images/2019/09/27/multimedia/27xp-spiderman/27xp-spiderman-superJumbo.jpg?quality=90&auto=webp"
+        }
+        key={2}
       />
     );
-    const titleElement = getByText("Here is a headline");
-    expect(titleElement).toBeInTheDocument();
+    const headline = getByText("Here is a headline");
+    const description = getByText("My description goes here");
+    expect(headline).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+    expect(getByText("Read more")).toBeInTheDocument();
+    debug();
   });
 });
