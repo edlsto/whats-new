57ee5921ec2a93930faf82f045d50d04c6b2b34c
Edward Stoner <edwardlstoner@gmail.com>
2020-03-18 19:55:12 -0600

    Add handling for bad image

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import "./NewsArticle.css";
 
-const NewsArticle = props => (
-  <div className="news-article">
-    <img src={props.img} alt="" />
-    <h2>{props.headline}</h2>
-    <p>{props.description}</p>
-    <button>
-      <a href={props.url}>Read more</a>
-    </button>
-  </div>
-);
+class NewsArticle extends Component {
+  constructor(props) {
+    super();
+  }
+
+  addDefaultSrc(e) {
+    e.target.src = "https://bitsofco.de/content/images/2018/12/broken-1.png";
+  }
+
+  render() {
+    return (
+      <div className="news-article">
+        <img src={this.props.img} onError={this.addDefaultSrc} alt="" />
+        <h2>{this.props.headline}</h2>
+        <p>{this.props.description}</p>
+        <button>
+          <a href={this.props.url}>Read more</a>
+        </button>
+      </div>
+    );
+  }
+}
 
 export default NewsArticle;
