5f256dcc16c7030bbfdd4c20126fef248ec20c53
Edward Stoner <edwardlstoner@gmail.com>
2020-03-19 16:38:09 -0600

    Remove comments and unused variable

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App.js";
 import "@testing-library/jest-dom";
 
 describe("App", () => {
   it("renders the text we expect", () => {
-    const { getByText, debug } = render(<App />);
+    const { getByText } = render(<App />);
     expect(getByText("What's New")).toBeInTheDocument();
   });
-
-  // it("selectTopic is called when the submit button is clicked", () => {
-  //   const mockSelectTopic = jest.fn();
-  //   const { getByText } = render(<Menu selectTopic={mockSelectTopic} />);
-  //   const localNews = getByText("Local news");
-  //   fireEvent.click(localNews);
-  //   expect(mockSelectTopic).toHaveBeenCalledWith("local");
-  // });
 });
